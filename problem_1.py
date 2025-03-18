48b1e65efcdafe64d501f0414b862d3cfde3fea9
SAHIL KHAN <jksahilkhan786@gmail.com>
2025-03-19 01:03:34 +0530

    Add files via upload

---
+++ problem_1.py
@@ -0,0 +1,9 @@
+words = {
+    "help": "madad",
+    "chair": "kursi",
+    "Rich": "poor"
+}
+
+word = input("Enter key whos want you want value: ")
+
+print(words[word])
---
+++ problem_6.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter Freind name :")
+lang = input("Enter fav Language :")
+d.update({name: lang})
+
+name = input("Enter Freind name :")
+lang = input("Enter fav Language :")
+d.update({name: lang})
+
+name = input("Enter freind name :")
+lang = input("Enter fav Language :")
+d.update({name: lang})
+
+name = input("Enter freind Name :")
+lang = input("Enter fav Language :")
+d.update({name: lang})
+
+print(d)
